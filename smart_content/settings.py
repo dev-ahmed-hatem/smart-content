395c6e2a8a487e40418834a6a7661c0d3f38b25f
Ahmed Hatem <ahmedhatemezzathelal@gmail.com>
2022-09-21 06:55:09 +0200

    media edited

--- smart_content/settings.py
+++ smart_content/settings.py
@@ -126,9 +126,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles/media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
